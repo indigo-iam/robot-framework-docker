d78e158976cd8f4ab7f4e9c5e5994c3c1e22ccf5
Federica Agostini <federica.agostini@cnaf.infn.it>
2025-03-04 15:26:51 +0100

    Build robot docker image from almalinux:9 (#2)

---
+++ docker/scripts/provide-robotframework.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 -m pip install --upgrade pip && \
+python3 -m pip install --upgrade robotframework && \
+python3 -m pip install --upgrade robotframework-httplibrary && \
+python3 -m pip install --upgrade robotframework-jsonlibrary && \
+python3 -m pip install --upgrade oic && \
+python3 -m pip install --upgrade requests && \
+python3 -m pip install --upgrade pyyaml && \
+python3 -m pip install --upgrade pyjwt
